152c877fb7e4d7790380241870085bf84928530b
David Novella <david@vineleaf.dev>
2023-08-29 19:53:11 +0200

    Initial Commit

---
+++ KnightIndustries Watch App/KnightIndustriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnightIndustriesApp.swift
+//  KnightIndustries Watch App
+//
+//  Created by David Novella Giménez on 29/8/23.
+//
+
+import SwiftUI
+
+@main
+struct KnightIndustries_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
